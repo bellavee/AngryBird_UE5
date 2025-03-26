693b8d42250a907a309ef0e8f278149ce6a14ee6
Vy Vu <74190308+bellavee@users.noreply.github.com>
2025-03-26 01:04:04 +0100

    init input actions, projectile, trajectory

---
+++ Source/AngryBird/Private/Projectile.cpp
@@ -0,0 +1,30 @@
+#include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AProjectile::AProjectile() {
+	PrimaryActorTick.bCanEverTick = true;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh"));
+	RootComponent = ProjectileMesh;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->InitialSpeed = 0.0f;
+	ProjectileMovement->MaxSpeed = 5000.0f;
+	ProjectileMovement->bRotationFollowsVelocity = false;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 1.0f;
+}
+
+void AProjectile::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AProjectile::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
+void AProjectile::Launch(FVector LaunchVelocity) {
+	ProjectileMovement->Velocity = LaunchVelocity;
+}
+
